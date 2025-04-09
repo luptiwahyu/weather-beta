f878e486b71700a0a69c4eb95d3160d76376b54c
Lupti Wahyu <lupti.utama@advotics.com>
2025-04-09 15:20:51 +0700

    feat: change style to scss

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 
 const welcome = ref('Hello Vue World!')
+const description = ref('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui eros, accumsan suscipit dapibus sed, convallis in ex. Mauris semper velit quis faucibus elementum.')
 
 setTimeout(() => {
   welcome.value = 'Gracias!'
@@ -9,8 +10,31 @@ setTimeout(() => {
 </script>
 
 <template>
-  <h1>{{ welcome }}</h1>
+  <div class="welcome">
+    <div class="welcome__title">{{ welcome }}</div>
+    <div class="welcome__description">{{ description }}</div>
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.welcome {
+  margin-left: auto;
+  margin-right: auto;
+  width: 500px;
+  background-color: yellow;
+  text-align: center;
+  padding: 20px;
+
+  &__title {
+    margin-bottom: 20px;
+    font-size: 30px;
+    font-weight: bold;
+    color: red;
+  }
+
+  &__description {
+    font-size: 24px;
+    color: var(--vt-c-black-mute);
+  }
+}
 </style>
